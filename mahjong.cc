1a51eac448dd467a8eff91a853de4abe48a3e9c7
ie715630 <51387755+ie715630@users.noreply.github.com>
2024-07-18 16:14:59 +0000

    Create tile instances

--- mahjong.cc
+++ mahjong.cc
@@ -3,56 +3,79 @@
 
 #include "mahjong.h"
 
-int main() {
-    printf("Welcome to mahjong\n\n");
+void MahjongSet::create_dragon_tiles() {
+    MahjongTile * red_dragon =  new MahjongTile(DRAGON, "Red");
+    MahjongTile * green_dragon =  new MahjongTile(DRAGON, "White");
+    MahjongTile * white_dragon =  new MahjongTile(DRAGON, "Green");
 
-    const TileGroup DRAGON = TileGroup{.name = "Dragon", .has_a_number = false};
-    const TileGroup WIND = TileGroup{.name = "Wind", .has_a_number = false};
-    const TileGroup FLOWER = TileGroup{.name = "Flower", .has_a_number = false};
-    const TileGroup SYMBOL = TileGroup{.name = "Symbol", .has_a_number = true};
-    const TileGroup BAMBOO = TileGroup{.name = "Bamboo", .has_a_number = true};
-    const TileGroup DOT = TileGroup{.name = "Dot", .has_a_number = true};
-
-    MahjongTile * red_dragon = new MahjongTile(DRAGON, "Red");
-    MahjongTile * green_dragon = new MahjongTile(DRAGON, "White");
-    MahjongTile * white_dragon = new MahjongTile(DRAGON, "Green");
-
+    int initial_tile_num = _num_tiles;
+    for (; (_num_tiles - initial_tile_num) < DRAGON.num_tiles_in_set;) {
+        _mahjong_set[_num_tiles++] = red_dragon;
+        _mahjong_set[_num_tiles++] = green_dragon;
+        _mahjong_set[_num_tiles++] = white_dragon;
+    }
+}
+void MahjongSet::create_wind_tiles() {
     MahjongTile * north = new MahjongTile(WIND, "North");
     MahjongTile * south = new MahjongTile(WIND, "South");
     MahjongTile * east = new MahjongTile(WIND, "East");
     MahjongTile * west = new MahjongTile(WIND, "West");
 
-    MahjongTile * winter = new MahjongTile(FLOWER, "Winter");
-    MahjongTile * autumn = new MahjongTile(FLOWER, "Autumn");
-    MahjongTile * fall = new MahjongTile(FLOWER, "Fall");
-    MahjongTile * spring = new MahjongTile(FLOWER, "Spring");
-
-    int kNumTiles = 9;
-
-    MahjongTileNumerical * symbols[9];
-    for (int tile_num = 0; tile_num < kNumTiles; tile_num++ ) {
-        int tile_number = tile_num + 1;
-        symbols[tile_num] = new MahjongTileNumerical(SYMBOL, tile_number);
+    int initial_tile_num = _num_tiles;
+    for (; (_num_tiles - initial_tile_num) < WIND.num_tiles_in_set;) {
+        _mahjong_set[_num_tiles++] = north;
+        _mahjong_set[_num_tiles++] = south;
+        _mahjong_set[_num_tiles++] = east;
+        _mahjong_set[_num_tiles++] = west;
     }
+}
 
-    MahjongTileNumerical * bamboos[9];
-    for (int tile_num = 0; tile_num < kNumTiles; tile_num++ ) {
-        int tile_number = tile_num + 1;
-        bamboos[tile_num] = new MahjongTileNumerical(BAMBOO, tile_number);
-    }
+void MahjongSet::create_flower_tiles() {
 
-    MahjongTileNumerical * dots[9];
-    for (int tile_num = 0; tile_num < kNumTiles; tile_num++ ) {
-        int tile_number = tile_num + 1;
-        dots[tile_num] = new MahjongTileNumerical(DOT, tile_number);
-    }
+}
 
-    red_dragon->print();
-    symbols[3]->print();
-    bamboos[4]->print();
-    dots[5]->print();
+void MahjongSet::create_symbol_tiles() {
+
+}
+void MahjongSet::create_bamboo_tiles() {
     
+}
+void MahjongSet::create_dot_tiles() {
+    
+}
 
+int main() {
+    printf("Welcome to mahjong\n\n");
+
+    MahjongSet mahjong_set = MahjongSet();
+    mahjong_set.print();
+
+    /*
+
+
+
+    int kNumValues = 9;
+    MahjongTileNumerical symbols[9];
+    for (int tile_num = 0; tile_num < kNumValues; tile_num++ ) {
+        int tile_number = tile_num + 1;
+        symbols[tile_num] = MahjongTileNumerical(SYMBOL, tile_number);
+    }
+
+    MahjongTileNumerical bamboos[9];
+    for (int tile_num = 0; tile_num < kNumValues; tile_num++ ) {
+        int tile_number = tile_num + 1;
+        bamboos[tile_num] = MahjongTileNumerical(BAMBOO, tile_number);
+    }
+
+    MahjongTileNumerical dots[9];
+    for (int tile_num = 0; tile_num < kNumValues; tile_num++ ) {
+        int tile_number = tile_num + 1;
+        dots[tile_num] = MahjongTileNumerical(DOT, tile_number);
+    }
+
+    red_dragon.print();
+    
+*/
     return 0;
 }
 
--- mahjong.h
+++ mahjong.h
@@ -3,20 +3,24 @@
 
 #include <stdio.h>
 #include<string>
+#include<list>
+
+#define TILES_IN_SET 144
 
 // enum class TileGroups {DRAGON, WIND, FLOWER, SYMBOL, BAMBOO, DOT};
 
 struct TileGroup {
     std::string name;
     bool has_a_number;
+    int num_tiles_in_set;
 };
 
 class MahjongTile {
-  protected:
+protected:
     TileGroup _tile_group;
     std::string _name;
 
-  public:
+public:
     // TODO: Clean constructors
     explicit MahjongTile() {}
     explicit MahjongTile(TileGroup tile_group, std::string name)
@@ -28,16 +32,56 @@ class MahjongTile {
 };
 
 class MahjongTileNumerical : public MahjongTile{
-  protected:
+protected:
     int _value;
 
-  public:
+public:
     // TODO: Clean constructors
+    explicit MahjongTileNumerical() {}
     explicit MahjongTileNumerical(TileGroup tile_group, int value) {
         _tile_group = tile_group;
         _value = value;
         _name = std::to_string(value);
     }
+};
+
+class MahjongSet {
+public:
+    MahjongSet() {
+        _num_tiles = 0;
+        create_dragon_tiles();
+        create_wind_tiles();
+        create_flower_tiles();
+        create_symbol_tiles();
+        create_bamboo_tiles();
+        create_dot_tiles();
+    }
+
+    void print() {
+        printf("******Mahjong set******\n");
+        for (int num_tile = 0; num_tile < TILES_IN_SET; num_tile++) {
+            _mahjong_set[num_tile]->print();
+        }
+    }
+
+protected:
+    MahjongTile * _mahjong_set [TILES_IN_SET];
+    int _num_tiles;
+
+    const TileGroup DRAGON = TileGroup{.name = "Dragon", .has_a_number = false, .num_tiles_in_set = 12};
+    const TileGroup WIND = TileGroup{.name = "Wind", .has_a_number = false, .num_tiles_in_set = 16};
+    const TileGroup FLOWER = TileGroup{.name = "Flower", .has_a_number = false, .num_tiles_in_set = 8};
+    const TileGroup SYMBOL = TileGroup{.name = "Symbol", .has_a_number = true, .num_tiles_in_set = 36};
+    const TileGroup BAMBOO = TileGroup{.name = "Bamboo", .has_a_number = true, .num_tiles_in_set = 36};
+    const TileGroup DOT = TileGroup{.name = "Dot", .has_a_number = true, .num_tiles_in_set = 36};
+    
+    void create_dragon_tiles();
+    void create_wind_tiles();
+    void create_flower_tiles();
+    void create_symbol_tiles();
+    void create_bamboo_tiles();
+    void create_dot_tiles();
+
 };
 
 #endif
