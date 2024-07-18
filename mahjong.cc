c7b0d1d3431e39c64c94bb07319177601b09f628
ie715630 <51387755+ie715630@users.noreply.github.com>
2024-07-18 08:33:32 +0000

    Tile classes

--- mahjong.cc
+++ mahjong.cc
@@ -2,10 +2,29 @@
 #define MAHJONG_CC
 
 #include "mahjong.h"
-#include <stdio.h>
 
 int main() {
     printf("Welcome to mahjong\n");
+
+    TileGroup dragon = TileGroup{.name = "Dragon", .has_a_number = false};
+    TileGroup wind = TileGroup{.name = "Wind", .has_a_number = false};
+    TileGroup flower = TileGroup{.name = "Flower", .has_a_number = false};
+    TileGroup symbol = TileGroup{.name = "Symbol", .has_a_number = true};
+    TileGroup bamboo = TileGroup{.name = "Bamboo", .has_a_number = true};
+    TileGroup dot = TileGroup{.name = "Dot", .has_a_number = true};
+
+    MahjongTile * red_dragon = new MahjongTile(dragon, "Red");
+    MahjongTile * green_dragon = new MahjongTile(dragon, "White");
+    MahjongTile * white_dragon = new MahjongTile(dragon, "Green");
+
+    MahjongTileNumerical * symbol_1 = new MahjongTileNumerical(symbol, 1);
+
+    red_dragon->print();
+    green_dragon->print();
+    white_dragon->print();
+    symbol_1->print();
+    
+
     return 0;
 }
 
--- mahjong.h
+++ mahjong.h
@@ -1,8 +1,43 @@
 #ifndef MAHJONG_H
 #define MAHJONG_H
 
+#include <stdio.h>
+#include<string>
 
+enum class TileGroups {Dragon, Wind, Flower, Symbol, Bamboo, Dot};
 
+struct TileGroup {
+    std::string name;
+    bool has_a_number;
+};
 
+class MahjongTile {
+  protected:
+    TileGroup _tile_group;
+    std::string _name;
+
+  public:
+    // TODO: Clean constructors
+    explicit MahjongTile() {}
+    explicit MahjongTile(TileGroup tile_group, std::string name)
+             : _tile_group(tile_group), _name(name) {}
+
+    void print() {
+        printf("%s %s\n", _name.c_str(), _tile_group.name.c_str());
+    }
+};
+
+class MahjongTileNumerical : public MahjongTile{
+  protected:
+    int _value;
+
+  public:
+    // TODO: Clean constructors
+    explicit MahjongTileNumerical(TileGroup tile_group, int value) {
+        _tile_group = tile_group;
+        _value = value;
+        _name = std::to_string(value);
+    }
+};
 
 #endif
